24a95d1cf7cddb65bc3e3b49863ad4c271a7d34c
Oleksii Vykhryst <19leshy77@gmail.com>
2022-10-22 19:57:21 +0300

    сделал разметку на 3 задачку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import { Profile } from "./Profile/Profile.jsx"
 import { Statistics } from "./Statistics/Statistics.jsx";
+import { FriendList } from "./FriendList/FriendList.jsx";
 import user from '../data/user.json/user'
 import data from '../data/data.json/data'
+import friends from '../data/friends.json/friends'
+
 
 export const App = () => {
   return (
@@ -19,7 +22,9 @@ export const App = () => {
     >
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends = {friends} />
+      
     </div>   
   );
 };
-console.log(App);
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+// import { getRandomHexColor } from 'utils/getRandomColor ';
+export const FriendList = ({ friends }) => {
+    console.log({friends});
+    return <ul className="friendList">{friends.map((item)=>
+        <li className="item" key={item.id}>
+            <span className="status">{item.isOnline}</span>
+            <img className="avatar" src={item.avatar} alt={item.name} width="48" />
+            <p className="name">{item.name}</p>
+        </li>)}
+</ul>
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.array.isRequired,
+    item: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+    })
+}
